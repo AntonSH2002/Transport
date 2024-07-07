20dcceaef16e07a92e4715b758554ba36a041f1a
AntonSH2002 <126949177+AntonSH2002@users.noreply.github.com>
2024-07-07 14:52:22 +0700

    Transport

---
+++ Transport/classes.h
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Transport
+{
+    public:
+        virtual void getInfo() = 0;
+
+    protected:
+        string transportName;
+        int maxSpeed;
+        int numberOfWheels;
+        int numberOfPassengers;
+};
+
+class Motorcycle : public Transport
+{
+    public:
+        Motorcycle()
+        {
+            this->transportName = "Мотоцикл";
+            this->maxSpeed = 150;
+            this->numberOfWheels = 2;
+            this->numberOfPassengers = 1;
+        }
+
+        void getInfo()
+        {
+            cout << "Наименование транспорта: " << this->transportName << endl;
+            cout << "Максимальная скорость: " << this->maxSpeed << "км/ч" << endl;
+            cout << "Количество колёс: " << this->numberOfWheels << endl;
+            cout << "Количество пассажиров: " << this->numberOfPassengers << endl << endl;
+        }
+};
+
+class Scooter : public Transport
+{
+    public:
+        Scooter()
+        {
+            this->transportName = "Самокат";
+            this->maxSpeed = 20;
+            this->numberOfWheels = 2;
+        }
+
+        void getInfo()
+        {
+            cout << "Наименование транспорта: " << this->transportName << endl;
+            cout << "Максимальная скорость: " << this->maxSpeed << "км/ч" << endl;
+            cout << "Количество колёс: " << this->numberOfWheels << endl << endl;
+        }
+};
+
+class Car : public Transport
+{
+    public:
+        Car()
+        {
+            this->transportName = "Автомобиль";
+            this->maxSpeed = 200;
+            this->numberOfWheels = 4;
+            this->numberOfPassengers = 4;
+        }
+
+        void getInfo()
+        {
+            cout << "Наименование транспорта: " << this->transportName << endl;
+            cout << "Максимальная скорость: " << this->maxSpeed << "км/ч" << endl;
+            cout << "Количество колёс: " << this->numberOfWheels << endl;
+            cout << "Количество пассажиров: " << this->numberOfPassengers << endl << endl;
+        }
+};
+
+class Factory
+{
+    public:
+        virtual Transport* createTransport() = 0;
+};
+
+class MotorcycleFactory : public Factory
+{
+    public:
+        Transport* createTransport()
+        {
+            return new Motorcycle;
+        }
+};
+
+class ScooterFactory : public Factory
+{
+    public:
+        Transport* createTransport()
+        {
+            return new Scooter;
+        }
+};
+
+class CarFactory : public Factory
+{
+    public:
+        Transport* createTransport()
+        {
+            return new Car;
+        }
+};
---
+++ Transport/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include "classes.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    #ifdef _WIN32
+        setlocale(LC_ALL, "Russian");
+    #else
+        setlocale(LC_ALL, "ru_RU.UTF-8");
+    #endif
+
+    if (argc < 2) {
+        cerr << "Используйте: " << argv[0] << " 1 2 ... N" << std::endl;
+        return 1;
+    }
+
+    unordered_map<string, unique_ptr<Factory>> factoryMap;
+    factoryMap["0"] = make_unique<MotorcycleFactory>();
+    factoryMap["1"] = make_unique<ScooterFactory>();
+    factoryMap["2"] = make_unique<CarFactory>();
+
+    vector<unique_ptr<Transport>> transports;
+    for (int i = 1; i < argc; i++)
+    {
+        string type = argv[i];
+        auto it = factoryMap.find(type);
+        if (it != factoryMap.end())
+        {
+            transports.push_back(unique_ptr<Transport>(it->second->createTransport()));
+        }
+        else
+        {
+            cerr << "Неизвстный тип транспорта: " << type << endl << endl;
+        }
+    }
+
+    for (const auto& transport : transports)
+    {
+        transport->getInfo();
+    }
+
+	return 0;
+}
